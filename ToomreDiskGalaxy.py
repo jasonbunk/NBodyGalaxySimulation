fdc6f07b5942b3fc953440bd4fd3d46f5a865076
Jason <jason.bunk@gmail.com>
2015-03-04 16:35:34 -0800

    Added stub Toomre Disk Galaxy

---
+++ ToomreDiskGalaxy.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from InitialConditions import InitialConditions
+
+
+class ToomreDiskGalaxy(InitialConditions):
+
+	def __init__(self):
+		InitialConditions.__init__(self)
+		#
+		#todo (see PlummerGalaxy for an example)
+		#
+	
+	
+	def GenerateInitialConditions(self, offset_x, offset_y, offset_z):
+		#
+		#todo (see PlummerGalaxy for an example)
+		#
+	
+	
+	def WriteToFile(self, outfilename):
+		#
+		#todo (see PlummerGalaxy for an example)
+		#
+
