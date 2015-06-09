70170bf52f977d501b0e50f75e9b214c8448a227
Jason <jason.bunk@gmail.com>
2015-06-09 14:45:54 -0700

    A

--- run_two_plummer_collision_MPI_and_OMP.py
+++ run_two_plummer_collision_MPI_and_OMP.py
@@ -1,4 +1,4 @@
-import os
+import os, sys, time
 import math
 import numpy as np
 from PlummerGalaxy import PlummerGalaxy
@@ -13,28 +13,42 @@ try:
 except ImportError:
     matplotlibAvailable = False
 
+if len(sys.argv) <= 1:
+	print("usage:  {prefix}  {total num pts}  {numMPIprocs}  {numCacheGrps}  {Nburst}")
+if len(sys.argv) >= 6:
+	preffix=sys.argv[1]
+	TotalNumPts = int(sys.argv[2])
+	numMPIprocs = sys.argv[3]
+	numMPImemgrps = sys.argv[4]
+	nburstburst = sys.argv[5]
+	print("using user-supplied arguments")
+else:
+	preffix="default"
+	TotalNumPts = 600
+	numMPIprocs = str( 6 )
+	numMPImemgrps = str( 2 ) #will be forced to be 2
+	nburstburst = str( 3 )
+	quit()
+
+GravitationalConst = 1.0
 
 # Settings:
 
 InitialConditionsFolder = "data/initialconditions/"
 OutputResultsFolder = "data/results/"
 
-GravitationalConst = 1.0
-
-TotalNumPts = 600
-numMPIprocs = str( 6 )
-numMPImemgrps = str( 2 ) #will be forced to be 2
-nburstburst = str( 3 )
-
-createNewInitialConditions = False
+createNewInitialConditions = True
 
 MakePositionsVideo = False
 UseImageMagickForFancierVideo = False
 
 #=================================================================================
 
+starttime = time.time()
+
 try:
     os.system('rm NBodySim_CPU_MPI/nbodycpumpi')
+    #os.system('rm data/initialconditions/*')
 except:
     print("executable already deleted")
 
@@ -63,22 +77,27 @@ if createNewInitialConditions:
     galaxy2.GenerateInitialConditions(4, 4, 0)
 
     if numMPImemgrps == '2':
-        galaxy1.WriteInitialConditionsToFile(InitialConditionsFolder+"two_plummers_collision_0.data", AarsethHeader)
-        galaxy2.WriteInitialConditionsToFile(InitialConditionsFolder+"two_plummers_collision_1.data", AarsethHeader)
+        galaxy1.WriteInitialConditionsToFile(InitialConditionsFolder+preffix+"0.data", AarsethHeader)
+        galaxy2.WriteInitialConditionsToFile(InitialConditionsFolder+preffix+"1.data", AarsethHeader)
     else:
         bothGalaxies = InitialConditions()
         bothGalaxies.extend(galaxy1)
         bothGalaxies.extend(galaxy2)
         AarsethHeader = str(TotalNumPts)+" 0.01 "+str(timeStep)+" "+str(timeMax)+" "+str(epssqd)+" "+str(GravitationalConst)+"\n"
-        bothGalaxies.WriteInitialConditionsToFile(InitialConditionsFolder+"two_plummers_collision_0.data", AarsethHeader)
+        bothGalaxies.WriteInitialConditionsToFile(InitialConditionsFolder+preffix+"0.data", AarsethHeader)
 	
         
 #args: {NumDataSplits}  {nburst-between-disksaves}  {InitialConditionsFilename-Prefix}  {OutputFilename-Prefix}
 
 print("Running compiled MPI/OpenMP C++ nbody code on two-Plummer-collision initial conditions files")
-os.system('mpirun -np '+numMPIprocs+' ./NBodySim_CPU_MPI/nbodycpumpi '+numMPImemgrps+' '+nburstburst+' '+InitialConditionsFolder+'two_plummers_collision_ '+OutputResultsFolder+'out_mpi_twocollisions_')
-	
+os.system('mpirun -np '+numMPIprocs+' ./NBodySim_CPU_MPI/nbodycpumpi '+numMPImemgrps+' '+nburstburst+' '+InitialConditionsFolder+preffix+' '+OutputResultsFolder+preffix)
 
+
+endtime = time.time()
+
+print("that took "+ (starttime - endtime)+" seconds")
+
+'''
 if True:
     print("launching renderer...")
     os.system("Renderer3D/Renderer3D "+OutputResultsFolder+"out_mpi_twocollisions_.data "+str(TotalNumPts/2)+" 0 1 1")
@@ -91,7 +110,7 @@ if matplotlibAvailable and (MakePositionsVideo or MakeDistributionsVideo):
     if MakePositionsVideo:
         MakeVideo(TotalNumPts, OutputResultsFolder+"out_MPI.data", "video_two_plummer_MPI_collision.avi", True, 8, UseImageMagickForFancierVideo)
 
-
+'''
 
 
 
